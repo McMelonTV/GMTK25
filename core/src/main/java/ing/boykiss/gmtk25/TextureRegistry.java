56c621c1e7f3f0bab4b14b40f2414c1005dc21a3
Anorak01 <adamfri007@gmail.com>
2025-07-31 01:33:54 +0200

    Added temporary player sheet registry

--- core/src/main/java/ing/boykiss/gmtk25/TextureRegistry.java
+++ core/src/main/java/ing/boykiss/gmtk25/TextureRegistry.java
@@ -4,4 +4,6 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class TextureRegistry {
     public static final Texture PLAYER_TEXTURE = new Texture("textures/player.png");
+
+    public static final Texture PLAYER_SHEET = new Texture("textures/player_sheet.png");
 }
