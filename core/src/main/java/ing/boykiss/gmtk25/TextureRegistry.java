f6565c134f72712c70e4a55b1960688923acb027
Anorak01 <adamfri007@gmail.com>
2025-07-31 00:34:51 +0200

    feat: TextureRegistry + player asset

---
+++ core/src/main/java/ing/boykiss/gmtk25/TextureRegistry.java
@@ -0,0 +1,7 @@
+package ing.boykiss.gmtk25;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class TextureRegistry {
+    public static final Texture PLAYER_TEXTURE = new Texture("textures/player.png");
+}
