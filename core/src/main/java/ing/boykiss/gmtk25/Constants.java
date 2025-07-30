03d4f881c33a5b77a07e633f71bfefd0d8bf2e61
Wh4I3-2 <154924333+Wh4I3-2@users.noreply.github.com>
2025-07-30 21:11:14 +0200

    viewport

--- core/src/main/java/ing/boykiss/gmtk25/Constants.java
+++ core/src/main/java/ing/boykiss/gmtk25/Constants.java
@@ -2,4 +2,6 @@ package ing.boykiss.gmtk25;
 
 public class Constants {
     public static final int TPS = 60;
+    public static final int VIEWPORT_WIDTH = 320;
+    public static final int VIEWPORT_HEIGHT = 180;
 }
--- core/src/main/java/ing/boykiss/gmtk25/GMTK25.java
+++ core/src/main/java/ing/boykiss/gmtk25/GMTK25.java
@@ -2,10 +2,17 @@ package ing.boykiss.gmtk25;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class GMTK25 extends ApplicationAdapter {
@@ -23,18 +30,51 @@ public class GMTK25 extends ApplicationAdapter {
         }
     });
 
+    private Viewport backViewport;
+    private Stage backStage;
+    private Image background;
+
+    private OrthographicCamera camera;
+    private Viewport viewport;
+    private Stage stage;
+
     @Override
     public void create() {
+        backViewport = new ScreenViewport();
+        backStage = new Stage();
+        backStage.setViewport(backViewport);
+
+        background = new Image(new Texture("textures/fill.png"));
+        background.setColor(Color.DARK_GRAY);
+        backStage.addActor(background);
+
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT, camera);
+        stage = new Stage();
+        stage.setViewport(viewport);
+
         tickThread.start();
     }
 
     @Override
     public void render() {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        backStage.draw();
+        stage.draw();
     }
 
     public void tick() {
+        backStage.act();
+        stage.act();
+    }
 
+    @Override
+    public void resize(int width, int height) {
+        backViewport.update(width, height);
+        viewport.update(width, height);
+        background.setSize(width, height);
+        background.setPosition(-width / 2.0f, -height / 2.0f);
     }
 
     @Override
