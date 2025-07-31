60e4e59e5f2d128362eddc0fe6aa51ee04a72b96
Wh4I3-2 <154924333+Wh4I3-2@users.noreply.github.com>
2025-07-31 16:19:05 +0200

    art: player jump + fall anim

--- core/src/main/java/ing/boykiss/gmtk25/AssetRegistry.java
+++ core/src/main/java/ing/boykiss/gmtk25/AssetRegistry.java
@@ -7,6 +7,8 @@ public class AssetRegistry {
 
     public static final Texture PLAYER_IDLE_TEXTURE = new Texture("textures/player_idle.png");
     public static final Texture PLAYER_RUN_TEXTURE = new Texture("textures/player_run.png");
+    public static final Texture PLAYER_JUMP_TEXTURE = new Texture("textures/player_jump.png");
+    public static final Texture PLAYER_FALL_TEXTURE = new Texture("textures/player_fall.png");
 
     public static final Texture FILL_TEXTURE = new Texture("textures/fill.png");
 }
