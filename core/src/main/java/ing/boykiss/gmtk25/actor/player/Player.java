aafb45d9fd4513ab55950c46075df0c4edf9b91f
Wh4I3 <154924333+Wh4I3-2@users.noreply.github.com>
2025-08-02 01:46:11 +0200

    fix: actually sane physics values

--- core/src/main/java/ing/boykiss/gmtk25/actor/player/Player.java
+++ core/src/main/java/ing/boykiss/gmtk25/actor/player/Player.java
@@ -39,9 +39,9 @@ public class Player extends Actor {
     @Getter
     private final EventHandler<Event> onJump = new EventHandler<>();
 
-    private static final int SPEED = 5000; // Speed of the player
-    private static final int JUMP_FORCE = 80; // Jump force of the player
-    private static final int MIN_JUMP_FORCE = 25; // Jump force of the player when jump is released
+    private static final float SPEED = 1250; // Speed of the player
+    private static final float JUMP_FORCE = 40; // Jump force of the player
+    private static final float MIN_JUMP_FORCE = 10; // Jump force of the player when jump is released
 
     private static final int COYOTE_TIME_DURATION = 6; // Duration of coyote time in ticks
     private boolean CoyoteTimeActive = false;
