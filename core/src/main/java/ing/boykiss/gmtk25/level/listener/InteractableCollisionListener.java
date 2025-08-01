78029e41984a146d948a7aa80f8580b2dcc2259e
Wh4I3 <154924333+Wh4I3-2@users.noreply.github.com>
2025-08-01 13:23:17 +0200

    feat: collision listeners use a space to seperate multiple tags

--- core/src/main/java/ing/boykiss/gmtk25/level/listener/InteractableCollisionListener.java
+++ core/src/main/java/ing/boykiss/gmtk25/level/listener/InteractableCollisionListener.java
@@ -4,16 +4,19 @@ import com.badlogic.gdx.physics.box2d.*;
 import ing.boykiss.gmtk25.actor.interactable.InteractableButton;
 import ing.boykiss.gmtk25.event.Event;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InteractableCollisionListener implements ContactListener {
     @Override
     public void endContact(Contact contact) {
         if (contact.getFixtureA().getUserData() instanceof String || contact.getFixtureB().getUserData() instanceof String) {
-            String fixtureA = contact.getFixtureA().getUserData() instanceof String ? (String) contact.getFixtureA().getUserData() : "unknown";
-            String fixtureB = contact.getFixtureB().getUserData() instanceof String ? (String) contact.getFixtureB().getUserData() : "unknown";
+            List<String> fixtureA = contact.getFixtureA().getUserData() instanceof String ? Arrays.stream(((String) contact.getFixtureA().getUserData()).split(" ")).toList() : List.of();
+            List<String> fixtureB = contact.getFixtureB().getUserData() instanceof String ? Arrays.stream(((String) contact.getFixtureB().getUserData()).split(" ")).toList() : List.of();
 
-            if (fixtureA.equals("button")) {
+            if (fixtureA.contains("button")) {
                 ((InteractableButton)contact.getFixtureA().getBody().getUserData()).getOnExit().call(new Event() {});
-            }  else if (fixtureB.equals("button")) {
+            }  else if (fixtureB.contains("button")) {
                 ((InteractableButton)contact.getFixtureB().getBody().getUserData()).getOnExit().call(new Event() {});
             }
         }
@@ -22,12 +25,12 @@ public class InteractableCollisionListener implements ContactListener {
     @Override
     public void beginContact(Contact contact) {
         if (contact.getFixtureA().getUserData() instanceof String || contact.getFixtureB().getUserData() instanceof String) {
-            String fixtureA = contact.getFixtureA().getUserData() instanceof String ? (String) contact.getFixtureA().getUserData() : "unknown";
-            String fixtureB = contact.getFixtureB().getUserData() instanceof String ? (String) contact.getFixtureB().getUserData() : "unknown";
+            List<String> fixtureA = contact.getFixtureA().getUserData() instanceof String ? Arrays.stream(((String) contact.getFixtureA().getUserData()).split(" ")).toList() : List.of();
+            List<String> fixtureB = contact.getFixtureB().getUserData() instanceof String ? Arrays.stream(((String) contact.getFixtureB().getUserData()).split(" ")).toList() : List.of();
 
-            if (fixtureA.equals("button")) {
+            if (fixtureA.contains("button")) {
                 ((InteractableButton)contact.getFixtureA().getBody().getUserData()).getOnEnter().call(new Event() {});
-            } else if (fixtureB.equals("button")) {
+            } else if (fixtureB.contains("button")) {
                 ((InteractableButton)contact.getFixtureB().getBody().getUserData()).getOnEnter().call(new Event() {});
             }
         }
