4763fd72e5cb98ab2ba4f7cb88df9011e68265cf
mel <mcmelon@nodiumhosting.com>
2025-08-03 01:08:12 +0200

    feat: draw paused text on paused screen (for some reason its offcenter tho)

--- core/src/main/java/ing/boykiss/gmtk25/actor/ui/PauseScreen.java
+++ core/src/main/java/ing/boykiss/gmtk25/actor/ui/PauseScreen.java
@@ -2,6 +2,7 @@ package ing.boykiss.gmtk25.actor.ui;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import ing.boykiss.gmtk25.GMTK25;
@@ -12,9 +13,15 @@ public class PauseScreen extends Actor {
     public void draw(Batch batch, float parentOpacity) {
         if (GMTK25.isPaused) {
             Color color = batch.getColor();
-            batch.setColor(0, 0, 0, 0.5f);
+
             // i have no clue why but i couldnt get this to work any other way so fuck it
+            batch.setColor(0, 0, 0, 0.5f);
             batch.draw(AssetRegistry.FILL_TEXTURE, -Gdx.graphics.getWidth(), -Gdx.graphics.getHeight(), Gdx.graphics.getWidth() * 2, Gdx.graphics.getHeight() * 2);
+
+            Texture pausedTextTexture = AssetRegistry.PAUSED_TEXT_TEXTURE;
+            batch.setColor(1, 1, 1, 1);
+            batch.draw(pausedTextTexture, -pausedTextTexture.getWidth() * 6, -pausedTextTexture.getHeight() * 6, pausedTextTexture.getWidth() * 6, pausedTextTexture.getHeight() * 6);
+
             batch.setColor(color);
         }
     }
