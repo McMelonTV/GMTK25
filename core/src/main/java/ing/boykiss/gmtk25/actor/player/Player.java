ad3d84b9019ea32e6332f6103619dbe123262cae
Anorak01 <adamfri007@gmail.com>
2025-08-03 18:17:27 +0200

    Made player start recording again after starting a new loop

--- core/src/main/java/ing/boykiss/gmtk25/actor/player/Player.java
+++ core/src/main/java/ing/boykiss/gmtk25/actor/player/Player.java
@@ -7,11 +7,7 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.Fixture;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import ing.boykiss.gmtk25.Constants;
 import ing.boykiss.gmtk25.GMTK25;
@@ -201,6 +197,8 @@ public class Player extends Actor {
         ReplayManager.INSTANCE.stopRecording();
         teleportToLevelStart();
         ReplayManager.INSTANCE.replay(this);
+        // start new recording
+        ReplayManager.INSTANCE.startRecording();
     }
 
     private final float spriteHeightOffset = (sprite.getHeight() * Constants.UNIT_SCALE) * 0.25f;
